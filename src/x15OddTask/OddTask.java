7d340f23977f2c6a3a2f8c70d560b8664052b430
SebastianRJames <rutherfordjamess@lordwandsworth.org>
2020-10-07 09:37:30 +0100

    odd task

--- src/x15OddTask/OddTask.java
+++ src/x15OddTask/OddTask.java
@@ -2,7 +2,7 @@ package x15OddTask;
 
 import java.util.Random;
 import java.util.Scanner;
-//   ---unfinished cant get random numbers to work but know how to check if odd or even.
+
 /*
  Task
      The user enters if they pick odd or even. A random number,
@@ -13,21 +13,32 @@ public class OddTask {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Do you choose an Odd number or an Even number? odd/even");
-        String oddEven = input.nextLine();
-        if ("odd".equals(oddEven)){// cannot use == for comparing string types, must use .equals
-            System.out.println("works");
-            Random randomOdd = new Random();
-            int rand1 = 0;
-            while (true){
-                rand1 = randomOdd.nextInt(7);
-                if (rand1 !=0) break;
-            System.out.println(rand1);
+        String oddEven = input.nextLine(); // oddEven = input
+        if ("odd".equals(oddEven)){// cannot use == for comparing string types, must use .equals, if picked even
+            Random rand = new Random(); // rand is the class
+            int upperbound = 7; // creating upperbound
+            int randomOdd = rand.nextInt(upperbound); // randomOdd = random number, with upperbound of 7
+            System.out.println("You rolled a "+randomOdd); // print random
+            // above is pick random, below is check if odd
+            if (randomOdd % 2 != 0) {// if num is NOT div by 2 then == Odd = return number
+                System.out.println(randomOdd + " is an Odd number!, You win!");
             }
-        }
-        if ("even".equals(oddEven)){
-            System.out.println("works");
-            Random rand = new Random();
-            int randomNum = rand.nextInt(7);
+            else {
+                System.out.println(randomOdd + " is an Even number, Sorry you loose.");
+            }
+            }
+        if ("even".equals(oddEven)){ // if picked even
+            Random rand1 = new Random();
+            int upperbound1 = 7;
+            int randomEven = rand1.nextInt(upperbound1);
+            System.out.println("You rolled a "+randomEven);
+            // above is pick random, below is check if even
+            if (randomEven % 2 == 0) {// if num IS div by 2 then == even = return number
+                System.out.println(randomEven + " is an Even number!, You win!");
+            }
+            else {
+                System.out.println(randomEven + " is an Odd number, Sorry you loose.");
+            }
         }
     }
 
