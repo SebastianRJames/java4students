3ddda8d7b270b89a6aed1263187615c3bcc9ed73
SebastianRJames <rutherfordjamess@lordwandsworth.org>
2020-11-10 09:05:22 +0000

    0.1

--- src/x24ArraysEx1/ArraysEx1.java
+++ src/x24ArraysEx1/ArraysEx1.java
@@ -9,16 +9,48 @@ public class ArraysEx1 {
      */
     public static void main(String[] args) {
             //int[]array = new int[9];
-            String [] arrName = {"s","e","b","a","s","t","i","a","n"};
-            System.out.println(arrName.length); // .length uses human numbers 1-9, not 0-8
+            //String [] arrName = {"s","e","b","a","s","t","i","a","n"};
+            //System.out.println(arrName.length); // .length uses human numbers 1-9, not 0-8
             // System.out.print(arrName[0]); System.out.print(arrName[1]);System.out.print(arrName[2]); // this works but doesn't use loops
-            //system.out.printf()
+
+//            for(int y=0;y<15;y++){ // limit to 15 characters, y is the length of the array
+//                System.out.println(y); // test for numbered letters
+//                int x = y; //
+//                if (x>=arrName.length) // if x is bigger or equal to arrayName's length
+//                    x=0; // reset x at 0
+//                System.out.print(arrName[x]);
+//
+//            }
+        //attempt 3
+        String [] nameArray = {"s","e","b","a","s","t","i","a","n"};
+        int lengthVar = nameArray.length; // aka size of array
+        int countingVar = 0; // aka x
+        // int i = 0;
+
+        for(int y = 0; y<lengthVar;lengthVar++){
+            for(countingVar = 0; countingVar<9; countingVar++){
+                if(countingVar>lengthVar){
+                    countingVar = countingVar-lengthVar;
+                    System.out.print(countingVar);
+                }
+                System.out.print(nameArray.charAt(countingVar));
+            }
+            System.out.println(""); // returns line
+        }
+
+
+
+
+
+
+
+
+
     }
 
-// sorry i have no idea
 }
 
-// us printf to make a box with the characters =
+// us printf to make a box with the characters = for next q
 //seb
 //ast
 //ian
--- src/x27Methods/Methods.java
+++ src/x27Methods/Methods.java
@@ -1,20 +1,67 @@
 package x27Methods;
 public class Methods {
     public static void main(String[] args) {
-
+            // aMethod();
+            ruleTheWorld();
+            rollTheDice();
+            chorusBells();
+            wickedWind();
+            chorusBells();
+            ohohoh();
+            chorusBells();
     }
-
+    private static void aMethod(){
+        System.out.println("Say hello");
+        System.out.println("Say goodbye");
+    }
     /*    Task
     Display the song Viva La Vida with each verse and the chorus
     as a method. Use as few different commands as possible to display the
     entire song
      */
-
-
-
-
-
-
+    private static void ruleTheWorld(){
+        System.out.print("I used to rule the world \nSeas would rise when I gave the word \nNow in the morning, I sleep alone\nSweep the streets I used to own\n\n");
+    }
+    private static void rollTheDice(){
+        System.out.print("I used to roll the dice\n" +
+                "Feel the fear in my enemy's eyes\n" +
+                "Listen as the crowd would sing\n" +
+                "Now the old king is dead! Long live the king!\n\n");
+    }
+    private static void heldTheKey(){
+        System.out.print("One minute I held the key\n" +
+                "Next the walls were closed on me\n" +
+                "And I discovered that my castles stand\n" +
+                "Upon pillars of salt and pillars of sand\n\n");
+    }
+    private static void chorusBells(){
+        System.out.print("I hear Jerusalem bells are ringing\n" +
+                "Roman Cavalry choirs are singing\n" +
+                "Be my mirror, my sword and shield\n" +
+                "My missionaries in a foreign field\n" +
+                "For some reason I can't explain\n" +
+                "Once you go there was never, never an honest word\n" +
+                "And that was when I ruled the world\n\n");
+    }
+    private static void wickedWind(){
+        System.out.print("It was a wicked and wild wind\n" +
+                "Blew down the doors to let me in\n" +
+                "Shattered windows and the sound of drums\n" +
+                "People couldn't believe what I'd become\n\n");
+    }
+    private static void revolutionaries(){
+        System.out.print("Revolutionaries wait\n" +
+                "For my head on a silver plate\n" +
+                "Just a puppet on a lonely string\n" +
+                "Oh, who would ever want to be king?\n\n");
+    }
+    private static void ohohoh(){
+        System.out.print("Oh, oh, oh, oh, oh\n" +
+                "Oh, oh, oh, oh, oh\n" +
+                "Oh, oh, oh, oh, oh\n" +
+                "Oh, oh, oh, oh, oh\n" +
+                "Oh, oh, oh, oh, oh\n\n");
+    }
 
 
 
